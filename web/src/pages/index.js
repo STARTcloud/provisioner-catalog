72f63d05e655b3edb4f8c7cd796f8c1d7c3d5c1f
MarkProminic <mark.gilbert@prominic.net>
2026-09-04 03:29:20 -0500

    feat: shared login, register and invitation pages

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,4 +1,6 @@
 export { default as AboutPage } from './AboutPage';
+export { authShape, returnToShape } from './auth';
+export { default as AuthShell, AuthAlert, AuthSpinner, InboxIcon } from './AuthShell';
 export { default as CollectionPage } from './CollectionPage';
 export { default as ConfirmModal } from './ConfirmModal';
 export {
@@ -21,6 +23,7 @@ export { createDeployControls, deployableVersion } from './deploy';
 export { default as DeprecationBanner } from './DeprecationBanner';
 export { CollapseButton } from './GroupHeading';
 export { default as HomePage } from './HomePage';
+export { default as InvitePage } from './InvitePage';
 export { default as ItemFacts } from './ItemFacts';
 export { default as ItemPage } from './ItemPage';
 export {
@@ -44,9 +47,12 @@ export {
   versionShape,
   visibilityOf,
 } from './itemShape';
+export { default as LoginPage } from './LoginPage';
 export { isManager, isMember, isOwner } from './membership';
 export { default as OrgPage } from './OrgPage';
 export { default as PageHeader } from './PageHeader';
+export { default as ProviderButtons } from './ProviderButtons';
 export { default as ProviderPage } from './ProviderPage';
+export { default as RegisterPage } from './RegisterPage';
 export { default as StatusChips } from './StatusChips';
 export { default as VersionPage } from './VersionPage';
